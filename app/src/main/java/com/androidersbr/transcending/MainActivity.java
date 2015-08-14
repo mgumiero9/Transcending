a9051652f76c74eff7da5b58e46715c4f64aad8c
mgumiero9 <mgumiero9@gmail.com>
2015-08-14 01:30:21 -0300

    testing Environment.getDataDirectory method

--- app/src/main/java/com/androidersbr/transcending/MainActivity.java
+++ app/src/main/java/com/androidersbr/transcending/MainActivity.java
@@ -1,10 +1,12 @@
 package com.androidersbr.transcending;
 
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String myAppDirectory = Environment.getDataDirectory().toString();
+        TextView myTextView = (TextView) findViewById(R.id.app_directory);
+        myTextView.setText(myAppDirectory);
         
     }
 
